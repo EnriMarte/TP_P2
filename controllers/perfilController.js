05e43fc58c8bd79195ba6b68af5ac265dcc2a158
Luis Maria Palacio <luismariapalacio@Luiss-Air.fibertel.com.ar>
2020-09-25 10:19:34 -0300

    Creando form de Modificar perfil y rutas

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -2,6 +2,9 @@ let perfilController = {
     mostrarPerfil: function(req, res, next) {
         res.render("miPerfil")
       },
+      mostrarModPerfil: function(req, res, next) {
+        res.render("modificarPerfil")
+      },
 }
 
 module.exports = perfilController;
--- routes/rutaPerfil.js
+++ routes/rutaPerfil.js
@@ -5,6 +5,6 @@ let perfilController = require("../controllers/perfilController");
 
 /* GET home page. */
 router.get('/', perfilController.mostrarPerfil);
-
+router.get('/modificar', perfilController.mostrarModPerfil)
 
 module.exports = router;
