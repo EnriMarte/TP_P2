07a4569b7f50de17878d9a31202ed87f0b4266be
Valentino <vcappelletti@udesa.edu.ar>
2020-11-02 11:25:28 -0300

    Agregue de nuevo models

---
+++ database/models/Posteos.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+    let alias = "posteos";
+
+    let cols = {
+
+        nombre: DataTypes.STRING,
+        apellido:  DataTypes.STRING, 
+        nombreUser: DataTypes.STRING,
+        fechaNacimiento: DataTypes.DATE,
+        mail: DataTypes.STRING,
+        password: DataTypes.STRING,
+        telefono: DataTypes.INTEGER
+    };
+
+    let config = {
+        tableName: "posteos",
+        timestamps: false
+    };
+
+    const posteos = sequelize.define(alias, cols, config);
+
+    // posteos.associate = function(models) {
+    //     posteos.hasMany(models.Comentarios, {
+    //         as: "comentarios",
+    //         foreignKey: "posteos_id",
+    //     });     
+    // }
+    // posteos.associate = function(models) {
+    //     posteos.belongsTo(models.Usuarios, {
+    //         as: "usuarios",
+    //         foreignKey: "usuarios_id",
+    //     });     
+    // }
+    return posteos;
+}
